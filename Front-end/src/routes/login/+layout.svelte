ac5dc7834e134c74afce0934f17ac6dc451da7c1
lucas-alencar <lucasalencar.rd@gmail.com>
2023-09-17 23:01:03 -0300

    Criando tela de cadastro

---
+++ Front-end/src/routes/login/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+    const SRC_BACKGROUND = "/src/img/coletaDeLeite.png";
+</script>
+
+<div class="relative flex flex-col w-full h-screen lg:flex-row justify-end">
+
+    <div class="grid rounded-none h-full width-card bg-gray-50 place-items-center absolute inset-0 custom-rounded">
+        <div class="custom-form w-2/5 flex flex-col items-center justify-center h-full">  
+            <slot />
+        </div>
+    </div>
+
+    <div class=" width-img">
+        <img src="{SRC_BACKGROUND}" alt= "Mulher segurando um extrator de leite com um bebê de fundo" class="object-cover h-full">
+    </div>
+
+</div>
+
+<style>
+    .custom-rounded {
+    border-top-right-radius: 40px;
+    border-bottom-right-radius: 40px;
+    }
+    .width-card{
+        width:40%;
+    }
+
+    .width-img{
+        width:62%;
+    }
+    .custom-form{
+        display: flex;
+        flex-direction: column;
+        justify-content: center; /* Centraliza verticalmente os itens */
+        align-items: center; /* Centraliza horizontalmente os itens */
+        gap: 4rem; /* Espaçamento maior entre os itens */
+    }
+</style>
---
+++ Front-end/src/routes/login/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+    import EmailIcon from 'svelte-material-icons/Email.svelte';
+    import FacebookIcon from 'svelte-material-icons/Facebook.svelte'
+    import InstagramIcon from 'svelte-material-icons/Instagram.svelte'
+    const SRC_LOGO = "/src/img/logo.png";
+</script>
+
+    
+        <img src="{SRC_LOGO}" alt="Logo do doama" class="object-cover  h-auto w-40">
+    
+        <div class="grid w-full">
+            <input type="text" placeholder="Usuário" class="input input-bordered w-full "/>  
+            <div class="form-control w-full">
+                <input type="password" placeholder="Senha" class="input input-bordered w-full mt-3"/>
+                <label class="label">
+                <span class="label-text-alt">
+                    <div class="form-control">
+                        <label class="label cursor-pointer">
+                          <input type="checkbox" class="checkbox h-5 w-5 mr-1" />
+                          <span class="label-text-alt">lembrar-me</span> 
+                        </label>
+                      </div>
+                </span>
+                <span class="underline label-text-alt">Esqueci minha senha</span>
+                </label>
+            </div>
+        </div>
+    
+        <div class="flex justify-between w-full">
+            <a class="width-button"href="/login/cadastro"><button class="btn btn-outline btn-success w-full" >Registrar-se</button></a>
+            <button class="btn btn-success text-white width-button">Entrar</button>
+        </div>
+    
+        <div class="w-full">
+            <div class="divider">Ou fazer login com</div>
+            <div class="flex items-center justify-center space-x-4">
+                <EmailIcon class='h-7 w-7'/>
+                <FacebookIcon class='h-7 w-7'/>
+                <InstagramIcon class='h-7 w-7'/>
+            </div>
+        </div>
+        
+    
+
+<style>
+    .width-button{
+        width:49%;
+    }
+</style>
